8946ebd3833c4e1f5fdb6fad7c865e6c872a88a1
nickolas da rocha machado <nickolas123full@gmail.com>
2020-09-07 22:31:13 -0300

    julia basics

---
+++ test.jl
@@ -0,0 +1,133 @@
+function name()
+   #code
+end
+
+"""
+   name()
+
+Description: 
+
+Usage: 
+```julia_repl
+julia>name()
+
+```
+"""
+function name()
+   
+end
+
+@inline name() = 
+
+"""
+   name()
+
+Description: 
+
+Usage: 
+```julia_repl
+julia>name()
+
+```
+"""
+@inline name() = 
+
+struct Name
+   #data
+end
+
+mutable struct Name
+   #data
+end
+
+@enum Color red blue green
+
+Array{Int, 3}(undef, (2,3,4))
+
+Array{Int, 1}(undef, 3)
+
+Array{Int, 1}(undef, 3, 3)
+
+BitArray{3}(undef, (3,3,3))
+
+BitArray{1}(undef, 5)
+
+module Name
+   #code
+end
+
+#=
+commenting
+=#
+
+"""
+   test(arg)
+
+Description: testing
+
+Usage: 
+```julia_repl
+julia>test(5)
+7
+```
+"""
+
+for i in 1:10
+   #code
+end
+
+if i == 5
+   testing
+else
+   testing
+end
+
+if x == 5
+   print
+else
+   print
+end
+
+if var == 1
+   a
+elseif var == 2
+   b
+else
+   c
+end
+
+if var == 1
+   a
+elseif var == 2
+   b
+elseif var == 3
+   c
+else
+   d
+end
+
+if var == 1
+   a
+elseif var == 2
+   b
+elseif var == 3
+   c
+elseif var == 4
+   d
+else
+   e
+end
+
+if var == 1
+   a
+elseif var == 2
+   b
+elseif var == 3
+   c
+elseif var == 4
+   d
+elseif var == 5
+   e
+else
+   f
+end
